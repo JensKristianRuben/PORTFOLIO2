2a01ddb8ae4aa9c89e846464558cd1c403c975e9
JensKristianRuben <Jenskristian@gmail.dk>
2025-10-03 18:16:30 +0200

    hover efftect added to the track

--- index.js
+++ index.js
@@ -2,37 +2,32 @@ const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
 const projectsContainer = document.getElementById("projectsContainer");
 
-// jeg vil gerne have fat i containeren der indeholder projekterne
-// jeg vil gerne flytte containeren hen ad x aksen når jeg klikker på knapperne
 
 
 let currentSpot = 0;
-const slideWidth = 200; // in pixels
+const slideWidth = 50; // in pixels
 
-previousButton.addEventListener("click", () => {
-    currentSpot += slideWidth;
-    console.log("prev button was clicked: ", currentSpot);
+previousButton.addEventListener("mouseenter", () => {
 
-    if (currentSpot <= 0) {
+    if (currentSpot === 0) {
         return;
     }
 
-
+    currentSpot += slideWidth;
+    console.log("prev button was clicked: ", currentSpot);
     projectsContainer.style.transform = `translateX(${currentSpot}px)`;
 
 
 });
 
-nextButton.addEventListener("click", () => {
-    currentSpot -= slideWidth;
-    console.log("Next button was clicked: ", currentSpot);
-
-
+nextButton.addEventListener("mouseenter", () => {
+    
     if (currentSpot <= -2000) {
         return;
     }
 
-
+    currentSpot -= slideWidth;
+    console.log("Next button was clicked: ", currentSpot);
 
     projectsContainer.style.transform = `translateX(${currentSpot}px)`;
 
